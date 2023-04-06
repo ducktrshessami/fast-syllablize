936deb6f95b030328707e638488565da738a6f0a
ducktrshessami <jwee761@gmail.com>
2023-04-05 17:53:19 -0700

    Initial build 529ee5a835e29aca201179a8675d23ddb65d7e4f

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+declare function syllablize(word: string): Array<string>;
+declare function methodA(word: string): Array<string>;
+declare function methodB(word: string): Array<string>;
+
+export { methodA, methodB, syllablize };
